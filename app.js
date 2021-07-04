2b0a2cb2616be956671bcb5d1928463374b48924
toluhikay <78743753+toluhikay@users.noreply.github.com>
2021-07-04 22:05:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const togglebutton = document.getElementsByClassName('togglebar')[0]
+const navbarlinks = document.getElementsByClassName('tac')[0]
+
+togglebutton.addEventListener('click', () =>{
+    tac.classlist.toggle('active')
+})
